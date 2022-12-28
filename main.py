2d9d1d41adf478ee67aad05c882ab25f196f0034
tomj <118008765+thomasjjj@users.noreply.github.com>
2022-12-28 19:05:16 +0000

    Version 0.1.1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import pandas as pd
 
-print("\nSIMPLE TELEGRAM WORDCLOUD GENERATOR Version 0.1.0\n"
+print("\nSIMPLE TELEGRAM WORDCLOUD GENERATOR Version 0.1.1\n"
       "Please add the exported csv to the\n"
       "directory and name it 'result.csv\n\n\n")
 
